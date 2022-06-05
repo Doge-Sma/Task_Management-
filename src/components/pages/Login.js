de526e0519a2c04f6a46b11b0b040ab92765b4cc
Annielle <anniellemedeiros@gmail.com>
2022-06-05 10:03:48 -0300

    posição certa imagem login

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,14 +1,22 @@
 import styles from "./Login.module.css"
 import logo from "../../imagens/logo.png"
 
-function Login(){
-    return(
+function Login() {
+    return (
 
         <div className={styles.login}>
             <h1> Bem- ao <span>Costs</span></h1>
-            <img src={logo} alt="Logo da Tela de Login" />
+            <img src={logo} alt="Logo da Tela de Login" className={styles.imagemLogo} />
+           
+            <div class="text">
+            <h1> Enjoy Your Time </h1>
+                <p1> When You Are Confused Abount </p1>
+                <p1> Managing Your Task Come To Us </p1>
+            </div>
+            <button> Login </button>
+            <button> Sign Up</button>
         </div>
-        
+
 
     );
 }
