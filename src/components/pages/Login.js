4517f44c5299d27e24ae6749718c8f3890db1313
Annielle <anniellemedeiros@gmail.com>
2022-06-04 21:23:15 -0300

    imagem da logo inserida com sucesso

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,12 @@
 import styles from "./Login.module.css"
+import logo from "../../imagens/logo.png"
 
 function Login(){
     return(
 
         <div className={styles.login}>
             <h1> Bem- ao <span>Costs</span></h1>
+            <img src={logo} alt="Logo da Tela de Login" />
         </div>
         
 
