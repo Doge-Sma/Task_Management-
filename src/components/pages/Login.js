269114a87d1b36d1b5226ff9f460218477cecacd
Annielle <anniellemedeiros@gmail.com>
2022-06-05 15:54:50 -0300

    tela de login feita

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -6,12 +6,13 @@ function Login() {
 
         <div className={styles.login}>
             <img src={logo} alt="Logo da Tela de Login" className={styles.imagemLogo} />
-           
-            <div class="text">
+
             <h1> Enjoy Your Time </h1>
+            <div class={styles.text}>
                 <p1> When You Are Confused Abount </p1>
                 <p1> Managing Your Task Come To Us </p1>
             </div>
+
             <button className={styles.buttonLogin}> Login </button>
             <button className={styles.buttonSign}> Sign Up</button>
 
